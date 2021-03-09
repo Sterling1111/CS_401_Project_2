b00991fd7fff9ca55f6fd32b8e977f81553c45e6
Sterling Jeppson <Sterlingijeppson@gmail.com>
2021-03-08 23:32:15 -0800

    Fixed a bug related to the TextField and JLabel alignment. When the main window was expanded they would lose allignment. This no longer happens. Updated and improved the user interface. Added the update button and functionality. Added database compatability for the initialize addressBook function. It no longer creats AddressEntry's button reads them in from database.

--- src/address/gui/AddressBook.java
+++ src/address/gui/AddressBook.java
@@ -1,23 +1,24 @@
 package address.gui;
 
 import address.data.AddressEntry;
+import address.data.Name;
+import address.data.Address;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
+import java.sql.*;
+import java.io.*;
 
-public class AddressBook extends JFrame implements ListSelectionListener{
+public class AddressBook extends JFrame implements ListSelectionListener {
 
     private JPanel panel1;
     private JTextField findTF;
-    private JButton deleteB;
-    private JButton addB;
     private JButton listB;
     private JButton findB;
     private JList<AddressEntry> addressEntryJList;
@@ -28,6 +29,8 @@ public class AddressBook extends JFrame implements ListSelectionListener{
     private JTextField stateTF;
     private JTextField zipTF;
     private JTextField emailTF;
+    private JTextField phoneTF;
+    private JLabel findL;
     private JLabel firstNameL;
     private JLabel lastNameL;
     private JLabel streetL;
@@ -35,14 +38,21 @@ public class AddressBook extends JFrame implements ListSelectionListener{
     private JLabel stateL;
     private JLabel zipL;
     private JLabel emailL;
-    private JTextField phoneTF;
     private JLabel phoneL;
-    private JLabel findL;
+    private JButton addB;
+    private JButton deleteB;
+    private JButton updateB;
     private ListModel listModel;
 
     private final TreeMap<String, TreeSet<AddressEntry>> addressEntryList = new TreeMap<>();
+    private String userName = "";
+    private String password = "";
+    private Connection conn = null;
+    private Statement stmt = null;
+    private PreparedStatement pstmt = null;
+    private ResultSet rset = null;
 
-    public AddressBook() {
+    public AddressBook() throws SQLException {
         setTitle("Address Book");
         setContentPane(panel1);
         setVisible(true);
@@ -50,19 +60,71 @@ public class AddressBook extends JFrame implements ListSelectionListener{
         this.pack();
         listModel = new ListModel();
         addressEntryJList.setModel(listModel);
-        initAddressBook();
         setEventHandlers();
+        setTextFieldsImmutable();
+        loadOracleJDBCDriver();
+        getCredentials();
+        initAddressBook();
     }
 
     public void initAddressBook() {
-        AddressEntry entry1 = new AddressEntry("Sterling", "Jeppson",
-                "2759 Vine Dr.","Livermore",
-                "CA", 94550, "sterlingijeppson@gmail.com", "925-289-6963", 1);
-        AddressEntry entry2 = new AddressEntry("D.S", "Malik",
-                "2759 Vine Dr.","Livermore",
-                "CA", 94550, "sterlingijeppson@gmail.com","925-289-6963", 2);
-        add(entry1);
-        add(entry2);
+        try {
+            conn = DriverManager.getConnection("jdbc:oracle:thin:" + userName + "/" + password + "@adcsdb01.csueastbay.edu:1521/mcspdb.ad.csueastbay.edu");
+            stmt = conn.createStatement();
+            rset = stmt.executeQuery("SELECT * FROM ADDRESSENTRYTABLE");
+            while(rset.next()) {
+                int ID = rset.getInt("ID");
+                String firstName = rset.getString("FIRSTNAME");
+                String lastName = rset.getString("LASTNAME");
+                String street = rset.getString("STREET");
+                String city = rset.getString("CITY");
+                String state = rset.getString("STATE");
+                int zip = rset.getInt("ZIP");
+                String email = rset.getString("EMAIL");
+                String phone = rset.getString("PHONE");
+                Name name = new Name(firstName, lastName);
+                Address address = new Address(street, city, state, zip);
+                AddressEntry ae = new AddressEntry(name, address, email, phone, ID);
+                this.add(ae);
+                listModel.add(ae);
+            }
+            rset.close();
+            stmt.close();
+            conn.close();
+        } catch(SQLException e) {
+            e.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                if(conn != null)
+                    conn.close();
+            } catch(SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        addressEntryJList.setSelectedIndex(0);
+    }
+
+    public void getCredentials() {
+        try {
+            File file = new File("credentials.txt");
+            Scanner inputFile = new Scanner(file);
+            userName = inputFile.nextLine();
+            password = inputFile.nextLine();
+        } catch(FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadOracleJDBCDriver() {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+        }
+        catch(ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     public void refreshList(TreeMap<String, TreeSet<AddressEntry>> addressEntryList) {
@@ -74,10 +136,11 @@ public class AddressBook extends JFrame implements ListSelectionListener{
         for(Map.Entry<String, TreeSet<AddressEntry>> entry : addressEntryList.entrySet()) {
             for(AddressEntry item : entry.getValue()) {
                 if(item != null) {
-                    listModel.add(item);
+                    listModel.add(new AddressEntry(item.getName(), item.getAddress(), item.getEmail(), item.getPhone(), item.getID()));
                 }
             }
         }
+        addressEntryJList.setSelectedIndex(0);
     }
 
     public void clearEntryTF() {
@@ -91,9 +154,8 @@ public class AddressBook extends JFrame implements ListSelectionListener{
         emailTF.setText("");
     }
 
-    public void add(AddressEntry entry) {
+    private void add(AddressEntry entry) {
         addressEntryList.computeIfAbsent(entry.getName().getLastName(), k -> new TreeSet<>()).add(entry);
-        listModel.add(entry);
     }
 
     private TreeSet<AddressEntry> getPrefixSet(String startOf_lastName) {
@@ -108,8 +170,7 @@ public class AddressBook extends JFrame implements ListSelectionListener{
     }
 
     public void valueChanged(ListSelectionEvent e) {
-        int index = addressEntryJList.getSelectedIndex();
-        System.out.println(index);
+        setTextFieldsImmutable();
         AddressEntry ae = listModel.getElementAt(addressEntryJList.getSelectedIndex());
         if(ae != null) {
             firstNameTF.setText(ae.getName().getFirstName());
@@ -121,6 +182,25 @@ public class AddressBook extends JFrame implements ListSelectionListener{
             phoneTF.setText(ae.getPhone());
             emailTF.setText(ae.getEmail());
         }
+    }
+
+    private void setTextFieldsMutability(boolean mutability) {
+        firstNameTF.setEditable(mutability);
+        lastNameTF.setEditable(mutability);
+        cityTF.setEditable(mutability);
+        stateTF.setEditable(mutability);
+        streetTF.setEditable(mutability);
+        zipTF.setEditable(mutability);
+        phoneTF.setEditable(mutability);
+        emailTF.setEditable(mutability);
+    }
+
+    public void setTextFieldsImmutable() {
+       setTextFieldsMutability(false);
+    }
+
+    public void setTextFieldsMutable() {
+        setTextFieldsMutability(true);
     }
 
     public AddressEntry constructEntryFromFields() {
@@ -140,20 +220,128 @@ public class AddressBook extends JFrame implements ListSelectionListener{
                 1);
     }
 
-    void setEventHandlers() {
-        addB.addActionListener(e -> {
-            AddressEntry ae = constructEntryFromFields();
+    public AddressEntry getElementAt(int index) {
+        for (Map.Entry<String, TreeSet<AddressEntry>> entry : addressEntryList.entrySet()) {
+            index -= entry.getValue().size();
+            if(index < 0) {
+                index = entry.getValue().size() - Math.abs(index);
+                for(AddressEntry elem : entry.getValue()) {
+                    if(index == 0)
+                        return elem;
+                    index--;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean removeElement(AddressEntry entry) {
+        String lastName = entry.getName().getLastName();
+        if(addressEntryList.computeIfPresent(lastName, (k, v) -> {
+            v.remove(entry);
+            if(v.isEmpty())
+                return null;
+            else return v;
+        }) != null) {
+            return true;
+        }
+        addressEntryList.remove(lastName);
+        return true;
+    }
+
+    public void setElementAt(AddressEntry entry, int index) {
+        AddressEntry ae = getElementAt(index);
+        if(entry.getName().getLastName().compareTo(ae.getName().getLastName()) != 0) {
+            System.out.println("They are not equal");
+            removeElement(ae);
+            ae.setName(entry.getName());
+            ae.setAddress(entry.getAddress());
+            ae.setEmail(entry.getEmail());
+            ae.setPhone(entry.getPhone());
             add(ae);
-        });
+        }
+        else {
+            ae.setName(entry.getName());
+            ae.setAddress(entry.getAddress());
+            ae.setEmail(entry.getEmail());
+            ae.setPhone(entry.getPhone());
+        }
+    }
 
-        findB.addActionListener(e -> {
-            TreeSet<AddressEntry> prefix_Set = getPrefixSet(findTF.getText());
-            TreeMap<String, TreeSet<AddressEntry>> addressEntryList = new TreeMap<>();
-            addressEntryList.put(findTF.getText(), prefix_Set);
-            refreshList(addressEntryList);
-        });
+    public int getIndexOf(AddressEntry ae) {
+        int index = 0;
+        for (Map.Entry<String, TreeSet<AddressEntry>> entry : addressEntryList.entrySet()) {
+            if(entry.getValue().contains(ae)) {
+                for(AddressEntry elem : entry.getValue()) {
+                    if(elem.equals(ae))
+                        return index;
+                    else
+                        index++;
+                }
+            }
+            index += entry.getValue().size();
+        }
+        return -1;
+    }
 
-        listB.addActionListener(e -> refreshList(this.addressEntryList));
+    public void setEventHandlers() {
+        setAddressEntryJListEventHandlers();
+        setDeleteBEventHandlers();
+        setFindBEventHandlers();
+        setFindTFEventHandlers();
+        setListBEventHandlers();
+        setUpdateBEventHandlers();
+        setAddBEventHandlers();
+    }
+
+    public void setAddressEntryJListEventHandlers() {
+        addressEntryJList.addListSelectionListener(this);
+    }
+
+    public void setDeleteBEventHandlers() {
+        deleteB.addActionListener(e -> {
+            setTextFieldsImmutable();
+            clearEntryTF();
+            AddressEntry ae = addressEntryJList.getSelectedValue();
+            if(ae == null)
+                return;
+            int index = addressEntryJList.getSelectedIndex();
+            boolean wasLastElement = false;
+            if(listModel.lastElement().compareTo(ae) == 0) {
+                wasLastElement = true;
+            }
+            String lastName = ae.getName().getLastName();
+            System.out.println(lastName);
+            if(addressEntryList.computeIfPresent(lastName, (k, v) -> {
+                v.remove(ae);
+                if(v.isEmpty())
+                    return null;
+                else return v;
+            }) == null) {
+                addressEntryList.remove(lastName);
+            }
+            addressEntryJList.clearSelection();
+            if(listModel.removeElement(ae)) {
+                clearEntryTF();
+                if(wasLastElement)
+                    addressEntryJList.setSelectedIndex(index - 1);
+                else
+                    addressEntryJList.setSelectedIndex(index);
+            }
+        });
+    }
+
+    void setFindTFEventHandlers() {
+        findTF.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if(addressEntryJList.getSelectedIndex() == -1) {
+                    clearEntryTF();
+                    setTextFieldsImmutable();
+                }
+            }
+            public void focusLost(FocusEvent e) {};
+        });
 
         findTF.addKeyListener(new KeyAdapter() {
             @Override
@@ -166,16 +354,62 @@ public class AddressBook extends JFrame implements ListSelectionListener{
                 }
             }
         });
+    }
 
-        deleteB.addActionListener(e -> {
-            AddressEntry ae = constructEntryFromFields();
-            addressEntryList.computeIfPresent(lastNameTF.getText(), (k, v) -> {v.remove(ae); return v;});
-            addressEntryJList.clearSelection();
-            if(listModel.removeElement(ae)) {
-                clearEntryTF();
+    public void setListBEventHandlers() {
+        listB.addActionListener(e -> {
+            setTextFieldsImmutable();
+            refreshList(this.addressEntryList);
+        });
+    }
+
+    public void setFindBEventHandlers() {
+        findB.addActionListener(e -> {
+            TreeSet<AddressEntry> prefix_Set = getPrefixSet(findTF.getText());
+            TreeMap<String, TreeSet<AddressEntry>> addressEntryList = new TreeMap<>();
+            addressEntryList.put(findTF.getText(), prefix_Set);
+            refreshList(addressEntryList);
+        });
+    }
+
+    public void setUpdateBEventHandlers() {
+        updateB.addActionListener(e -> {
+            if(!addressEntryJList.isSelectionEmpty() && firstNameTF.isEditable()) {
+                int index = addressEntryJList.getSelectedIndex();
+                AddressEntry currentlySelectedEntry = addressEntryJList.getSelectedValue();
+                AddressEntry entry = constructEntryFromFields();
+                setElementAt(entry, getIndexOf(currentlySelectedEntry));
+                listModel.setElementAt(entry, index);
+                addressEntryJList.setSelectedIndex(listModel.getIndexOf(entry));
+                setTextFieldsImmutable();
+            }
+            else if(!addressEntryJList.isSelectionEmpty()) {
+                setTextFieldsMutable();
+            }
+            else {
+                setTextFieldsImmutable();
             }
         });
-
-        addressEntryJList.addListSelectionListener(this::valueChanged);
     }
+
+    public void setAddBEventHandlers() {
+        addB.addActionListener(e -> {
+            if(firstNameTF.isEditable()) {
+                AddressEntry ae = constructEntryFromFields();
+                add(ae);
+                listModel.add(new AddressEntry(ae.getName(), ae.getAddress(), ae.getEmail(), ae.getPhone(), ae.getID()));
+                setTextFieldsImmutable();
+                addressEntryJList.setSelectedIndex(listModel.getIndexOf(ae));
+            }
+            else {
+                clearEntryTF();
+                addressEntryJList.clearSelection();
+                setTextFieldsMutable();
+                firstNameTF.requestFocus();
+            }
+        });
+    }
+
 }
+
+
--- src/address/gui/ListModel.java
+++ src/address/gui/ListModel.java
@@ -35,9 +35,45 @@ public class ListModel extends AbstractListModel<AddressEntry> {
         return null;
     }
 
+    public int getIndexOf(AddressEntry ae) {
+        int index = 0;
+        for (Map.Entry<String, TreeSet<AddressEntry>> entry : addressEntryList.entrySet()) {
+            if(entry.getValue().contains(ae)) {
+                for(AddressEntry elem : entry.getValue()) {
+                    if(elem.equals(ae))
+                        return index;
+                    else
+                        index++;
+                }
+            }
+            index += entry.getValue().size();
+        }
+        return -1;
+    }
+
     public void add(AddressEntry entry) {
         if(addressEntryList.computeIfAbsent(entry.getName().getLastName(), k -> new TreeSet<>()).add(entry))
             fireContentsChanged(this, 0, getSize());
+    }
+
+    public void setElementAt(AddressEntry entry, int index) {
+        AddressEntry ae = getElementAt(index);
+        if(entry.getName().getLastName().compareTo(ae.getName().getLastName()) != 0) {
+            System.out.println("They are not equal");
+            removeElement(ae);
+            ae.setName(entry.getName());
+            ae.setAddress(entry.getAddress());
+            ae.setEmail(entry.getEmail());
+            ae.setPhone(entry.getPhone());
+            add(ae);
+        }
+        else {
+            ae.setName(entry.getName());
+            ae.setAddress(entry.getAddress());
+            ae.setEmail(entry.getEmail());
+            ae.setPhone(entry.getPhone());
+        }
+        fireContentsChanged(this, 0, getSize());
     }
 
     public void addAll(AddressEntry[] elements) {
@@ -76,10 +112,18 @@ public class ListModel extends AbstractListModel<AddressEntry> {
 
     public boolean removeElement(AddressEntry entry) {
         String lastName = entry.getName().getLastName();
-        if(addressEntryList.computeIfPresent(lastName, (k, v) -> {v.remove(entry); return v;}) != null) {
+        if(addressEntryList.computeIfPresent(lastName, (k, v) -> {
+            v.remove(entry);
+            if(v.isEmpty())
+                return null;
+            else return v;
+        }) != null) {
             fireContentsChanged(this, 0, getSize());
             return true;
         }
-        return false;
+        System.out.println("The set is empty so lets remove it");
+        addressEntryList.remove(lastName);
+        fireContentsChanged(this, 0, getSize());
+        return true;
     }
 }
