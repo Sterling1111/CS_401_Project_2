e4dc6c9a5a14b605401b1b6eb068e8a2924d96c6
Karthikeyan Vijayaraj <karth8vader@gmail.com>
2021-03-12 15:42:47 -0800

    test comment

--- src/address/AddressBookApplication.java
+++ src/address/AddressBookApplication.java
@@ -5,7 +5,7 @@ import java.awt.EventQueue;
 
 public class AddressBookApplication {
     public static void main(String[] args) {
-
+    //test comment
         EventQueue.invokeLater(new Runnable() {
 
             public void run() {
